74ee3197888be37da987799eba85a62c0ec0ce25
gitZhekos <aigubov.zheka@yandex.ru>
2020-05-18 01:35:08 +0300

    Updates

---
+++ precache-manifest.705fc267ff3e71e78f3765266ed65ba1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df13df495de6847624662b5c9dffd3f5",
+    "url": "/chat_react/index.html"
+  },
+  {
+    "revision": "b19e2857ce66bf0a9f29",
+    "url": "/chat_react/static/css/2.491a7481.chunk.css"
+  },
+  {
+    "revision": "8fb95f206ef8738a8a88",
+    "url": "/chat_react/static/css/main.41879f39.chunk.css"
+  },
+  {
+    "revision": "b19e2857ce66bf0a9f29",
+    "url": "/chat_react/static/js/2.09e7651b.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/chat_react/static/js/2.09e7651b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb95f206ef8738a8a88",
+    "url": "/chat_react/static/js/main.ce0a94b4.chunk.js"
+  },
+  {
+    "revision": "675b09e675ecd01893e8",
+    "url": "/chat_react/static/js/runtime-main.3c1d4be4.js"
+  }
+]);
